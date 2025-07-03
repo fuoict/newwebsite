b0b0a374de484f1c3e2e1c00ef6878a111d1aa18
fuoict <ict@fuo.edu.ng>
2025-07-03 04:17:04 -0700

    Board of Trustee

--- resources/views/pages/theuniversity/about-board-of-trustee.blade.php
+++ resources/views/pages/theuniversity/about-board-of-trustee.blade.php
@@ -7,10 +7,195 @@
                 <div class="banner-spacing">
                     <div class="section-info">
                         <h2 data-aos="fade-up" data-aos-delay="100">Board of Trustees</h2>
-                        <p data-aos="fade-up" data-aos-delay="200">Get Started with Access to Issues of Fountain Chronicles</p>
+                        {{-- <p data-aos="fade-up" data-aos-delay="200">Get Started with Access to Issues of Fountain Chronicles</p> --}}
                     </div>
                 </div>
             </div>
         </div>
         <!-- End Section Banner Area -->
+         <div class="courses-area ptb-100">
+                <div class="container">
+                    <div class="section-title" data-aos="fade-up" data-aos-delay="100" style="text-align: left">
+                        <h3>Introduction</h3>
+                                <p>The part 1 Section 4 (2) of University Law (2007) provides that the power of the University shall be exercisable on behalf of the University by the Board of Trustees or 
+                                    Council or Senate and in any other manners be exercisable 
+                                    by the Senate with the approval of the Council and NUC.
+                                    The Law also provides that the proprietor shall appoint a body known as 
+                                    Board of Trustees who shall hold office 
+                                    for a five-year term renewable for another final term of five years.
+                                </p>
+                                <div>
+                                    <p><strong>Functions of the Board</strong></p>
+                                </div>
+                                <p>The functions of the Board of Trustees as entrenched in the University Law is as follows:</p>
+                                <ul>
+                                    <p>1. Holding in trust all permanent assets and properties of the University</p>
+                                    <p>2. Approve periodic strategic plans for the University</p>
+                                    <p>3. Approve policies and symbols of long-term application for the University</p>
+                                    <p>4. Appoint a Chancellor on personal recognition of experience, skill, and high integrity</p>
+                                    <p>5. Appoint a Pro-Chancellor who shall be a person of high integrity and renown for management competency</p>
+                                    <p>6. Appoint members of the Governing Council in line with provisions of the third schedule</p>
+                                    <p>7. Undertake periodic visitations to the University</p>
+                                    <p>8. Act as final decision authority in matters of interpretation 
+                                        of statutes governing the workings of the University and disputes 
+                                        between its functional organs and assist the University in soliciting and securing patrons, 
+                                        endowments, sponsorships, external recognitions and accreditations
+                                    </p>
+                                </ul>
+                        <div class="sub-title">
+                            <p>Board of Trustees</p>
+                        </div>
+                         <h2 class="title-anim" style="text-align:center">Members of the Board</h2>
+                    </div>
+                    <div class="col-lg-12">
+                        <div class="row">
+
+                            <div class="col-lg-4 col-sm-6 col-md-6 mb-4">
+                                <div class="course-item">
+                                    <div class="image">
+                                        {{-- <img src="{{ asset('img/events/sixt-inaugural-lecture.jpg') }}" style="height:250px" alt="image"> --}}
+                                    </div>
+                                    <div class="content">
+                                        <p>Professor (Mallam) Yusuf Olaolu Ali</p>
+                                        <p><strong>Chairman</strong></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-sm-6 col-md-6 mb-4">
+                                <div class="course-item">
+                                    <div class="image">
+                                        {{-- <img src="{{ asset('img/events/sixt-inaugural-lecture.jpg') }}" style="height:250px" alt="image"> --}}
+                                    </div>
+                                    <div class="content">
+                                        <p>Alhaji Kabeer Alaraba</p>
+                                        <p><strong>Secretary</strong></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-sm-6 col-md-6 mb-4">
+                                <div class="course-item">
+                                    <div class="image">
+                                        {{-- <img src="{{ asset('img/events/sixt-inaugural-lecture.jpg') }}" style="height:250px" alt="image"> --}}
+                                    </div>
+                                    <div class="content">
+                                        <p>Dr. AWA Ibraheem  </p>
+                                        <p><strong>Member</strong></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-sm-6 col-md-6 mb-4">
+                                <div class="course-item">
+                                    <div class="image">
+                                        {{-- <img src="{{ asset('img/events/sixt-inaugural-lecture.jpg') }}" style="height:250px" alt="image"> --}}
+                                    </div>
+                                    <div class="content">
+                                        <p>Prof. Is-haq O. Oloyede  </p>
+                                        <p><strong>Member</strong></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-sm-6 col-md-6 mb-4">
+                                <div class="course-item">
+                                    <div class="image">
+                                        {{-- <img src="{{ asset('img/events/sixt-inaugural-lecture.jpg') }}" style="height:250px" alt="image"> --}}
+                                    </div>
+                                    <div class="content">
+                                        <p>Alhaji Muritadha Adeniji  </p>
+                                        <p><strong>Member</strong></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-sm-6 col-md-6 mb-4">
+                                <div class="course-item">
+                                    <div class="image">
+                                        {{-- <img src="{{ asset('img/events/sixt-inaugural-lecture.jpg') }}" style="height:250px" alt="image"> --}}
+                                    </div>
+                                    <div class="content">
+                                        <p>Dr Foluke Kafayat ABDULRAZAQ, OON  </p>
+                                        <p><strong>Member</strong></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-sm-6 col-md-6 mb-4">
+                                <div class="course-item">
+                                    <div class="image">
+                                        {{-- <img src="{{ asset('img/events/sixt-inaugural-lecture.jpg') }}" style="height:250px" alt="image"> --}}
+                                    </div>
+                                    <div class="content">
+                                        <p>Alhaji B. Tajudeen Salau  </p>
+                                        <p><strong>Member</strong></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-sm-6 col-md-6 mb-4">
+                                <div class="course-item">
+                                    <div class="image">
+                                        {{-- <img src="{{ asset('img/events/sixt-inaugural-lecture.jpg') }}" style="height:250px" alt="image"> --}}
+                                    </div>
+                                    <div class="content">
+                                        <p>Professor Ganiyu O. Olatunde  </p>
+                                        <p><strong>Member</strong></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-sm-6 col-md-6 mb-4">
+                                <div class="course-item">
+                                    <div class="image">
+                                        {{-- <img src="{{ asset('img/events/sixt-inaugural-lecture.jpg') }}" style="height:250px" alt="image"> --}}
+                                    </div>
+                                    <div class="content">
+                                        <p>Alhaji AbdulRauf A. AbdulWaheed  </p>
+                                        <p><strong>Member</strong></p>
+                                    </div>
+                                </div>
+                            </div>
+                             <div class="col-lg-4 col-sm-6 col-md-6 mb-4">
+                                <div class="course-item">
+                                    <div class="image">
+                                        {{-- <img src="{{ asset('img/events/sixt-inaugural-lecture.jpg') }}" style="height:250px" alt="image"> --}}
+                                    </div>
+                                    <div class="content">
+                                        <p>Imam Moruf O. AbdulAzeez</p>
+                                        <p><strong>Member</strong></p>
+                                    </div>
+                                </div>
+                            </div>
+                             <div class="col-lg-4 col-sm-6 col-md-6 mb-4">
+                                <div class="course-item">
+                                    <div class="image">
+                                        {{-- <img src="{{ asset('img/events/sixt-inaugural-lecture.jpg') }}" style="height:250px" alt="image"> --}}
+                                    </div>
+                                    <div class="content">
+                                        <p>Alhaji Olalekan Saliu</p>
+                                        <p><strong>Member</strong></p>
+                                    </div>
+                                </div>
+                            </div>
+                             <div class="col-lg-4 col-sm-6 col-md-6 mb-4">
+                                <div class="course-item">
+                                    <div class="image">
+                                        {{-- <img src="{{ asset('img/events/sixt-inaugural-lecture.jpg') }}" style="height:250px" alt="image"> --}}
+                                    </div>
+                                    <div class="content">
+                                        <p>Vice-Admiral Jubrila Ayinla (Rtd)</p>
+                                        <p><strong>Member</strong></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-sm-6 col-md-6 mb-4">
+                                <div class="course-item">
+                                    <div class="image">
+                                        {{-- <img src="{{ asset('img/events/sixt-inaugural-lecture.jpg') }}" style="height:250px" alt="image"> --}}
+                                    </div>
+                                    <div class="content">
+                                        <p>Dr. Muiz Banire</p>
+                                        <p><strong>Member</strong></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
 @endsection
